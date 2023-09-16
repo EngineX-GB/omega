5fde954b2959480521c8565b1bcec3af495cc991
EngineX-GB <uk.engine.x@gmail.com>
2023-09-16 14:52:43 -0500

    Updated installation script

--- scripts/installer.ps1
+++ scripts/installer.ps1
@@ -8,15 +8,15 @@ Write-Output($INSTALL_DIR)
 Write-Output("Extracting files")
 Expand-Archive -Path "omega-cmd.zip" -DestinationPath $INSTALL_DIR\omega-cmd -Force
 
+$INSTALL_DIR = $INSTALL_DIR + "\omega-cmd"
+
 $CONTENT = "SET JAVA_HOME=" + $JAVA_HOME +
             "`n" +
             "SET INSTALL_DIR=" + $INSTALL_DIR +
             "`n" +
             "SET PATH=%PATH%;%JAVA_HOME%\bin;"
 
-# unzipn the contents in the INSTALL_DIR
-
-$INSTALL_DIR = $INSTALL_DIR + "\omega-cmd"
+# unzip the contents in the INSTALL_DIR
 
 # create a setenv.bat file and add it to the INSTALL_DIR path
 Set-Content -Path $INSTALL_DIR\setenv.bat -Value $CONTENT

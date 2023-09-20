4578ce2cecb65da718a2f1d507317b552c57462e
EngineX-GB <uk.engine.x@gmail.com>
2023-09-20 16:39:49 -0500

    Added version script and updated pom.xml

--- versioning.bat
+++ versioning.bat
@@ -1,14 +1,18 @@
 call C:\Users\rm_82\Documents\setenv.bat
 
-mvn help:evaluate -Dexpression=project.version -q -DforceStdout > target/version.txt
-
 rem Set the release version
-mvn build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion} versions:commit
+call mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.incrementalVersion} versions:commit
 
 rem Run a build
-mvn clean install -Dmaven.test.skip=true -P prod
+call mvn clean install -Dmaven.test.skip=true -P prod
+
+call mvn help:evaluate -Dexpression=project.version -q -DforceStdout > target/version.txt
+
+for /f %%a in (target/version.txt) do set output=%%a
 
 rem Placeholder to run git tag
+git tag -a "%output%" -m "Version %output%"
+git push origin %output%
 
 rem Set the new snapshot version
-mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
+call mvn build-helper:parse-version versions:set -DnewVersion=${parsedVersion.majorVersion}.${parsedVersion.minorVersion}.${parsedVersion.nextIncrementalVersion}-SNAPSHOT versions:commit
